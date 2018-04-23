d8afba62744d248f1313f1cbc22aad5e6343c730
Delaporte <delaportepatrick@ymail.com>
2018-04-23 09:20:07 +0200

    projet initialisé

---
+++ src/Controller/FormController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Patrick
+ * Date: 20/04/2018
+ * Time: 14:59
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class FormController extends Controller
+{
+
+    public function index(){
+        return $this->render('formulaire.html.twig');
+    }
+}
